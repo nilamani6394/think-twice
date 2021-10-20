2105aa49535300a01bcd0bc4b0438cd8eb95f2a4
imac <nilmani.j@elaunchinfotech.in>
2021-10-20 15:21:21 +0530

    Banner added success

---
+++ src/main/kotlin/com/nilmani/thinktwice/ThinkTwiceApplication.kt
@@ -0,0 +1,11 @@
+package com.nilmani.thinktwice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThinkTwiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThinkTwiceApplication>(*args)
+}
